c7629e5467741b3db3fd491425334cfe699945dc
maximehk <maxime.hacker@gmail.com>
2017-02-25 18:48:27 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ work_dir=$(dirname $(readlink -f $0))
 cd "${work_dir}"
 sudo apt-get update && cat packages.txt | xargs sudo apt-get install -y
 sudo chsh -s $(which zsh) ${USER}
-vcsh clone https://github.com/maximehk/zsh.git zsh
+vcsh clone https://github.com/maximehk/mr.git mr
+mr update
 
 find $work_dir/apps -name '*.sh' -type f -print0 | xargs -0 -n1 bash 
