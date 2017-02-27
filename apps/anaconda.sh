c720f3a0adbf56a01491f977b9abac41f2543c6a
maximehk <maxime.hacker@gmail.com>
2017-02-27 11:06:45 +0100

    Update anaconda.sh

--- apps/anaconda.sh
+++ apps/anaconda.sh
@@ -3,15 +3,20 @@
 work_dir=$(dirname $(readlink -f $0))
 cd "${work_dir}"
 
-bin_file=$(curl https://repo.continuum.io/archive/index.html 2>/dev/null | perl -lane 'if (/(Anaconda3.*?Linux-x86_64.sh)/) { print $1; exit 0; }')
+# Download the latest version of Anaconda 3
+versions_url="https://repo.continuum.io/archive/index.html"
+bin_file=$(curl ${versions_url} 2>/dev/null | perl -lane 'if (/(Anaconda3.*?Linux-x86_64.sh)/) { print $1; exit 0; }')
 download_link="https://repo.continuum.io/archive/$bin_file"
 
 echo "Downloading $download_link"
 curl -o "${bin_file}" "${download_link}"
 
-chmod +x "${bin_file}"
+# Install it
+mkdir -p $HOME/.local
+chmod +x "${work_dir}/${bin_file}"
 "${work_dir}/${bin_file}" -p "$HOME/.local/anaconda3" -b
 
+# Cleanup
 rm -f "${bin_file}"
 cd -
 
